4ef3c67ab96d800acb1d1cca9668185d1ed54b38
Grace Chung <hjngy0511@gmail.com>
2020-11-29 22:57:01 +0900

    #10/:bulb:/Post Schedules Common API/데이터 insert 까지 완료 done1 - client에서 오는 형태를 가정하고 post 요청시 schedules_common 테이블에 해당 데이터가 잘 insert todo - schedules_date에 post요청이 바로 연결되어서 가기때문에 날짜 계산을 위한 startdate, enddate, cycle, time을 응답으로 주어야한다. 그래서 응답을 json 형식으로 보내는 것

---
+++ app/main/service/schedules_common.py
@@ -0,0 +1,51 @@
+#-*- coding: utf-8 -*-
+# medicines 테이블에 관련된 쿼리문 작성하는 파일
+from flask import request, jsonify, redirect
+from flask_restx import Resource, fields, marshal
+from sqlalchemy import and_
+import json
+import jwt
+from datetime import time
+from operator import itemgetter
+from app.main import db
+from app.main.model.schedules_common import Schedules_common
+from app.main.model.users import Users
+from ..config import jwt_key, jwt_alg
+
+def post_schedules_common(data):
+  """ Post Common information of alarm"""
+  try:
+    token = request.headers.get('Authorization')
+    decoded_token = jwt.decode(token, jwt_key, jwt_alg)
+    user_id = decoded_token['id']
+
+    if decoded_token:
+      new_schedules_common = Schedules_common(
+        title=data['title'], 
+        memo=data['memo'],
+        startdate=data['startdate'],
+        enddate=data['enddate'],
+        cycle=data['cycle'],
+        user_id=user_id,
+        )
+      print('new_schedules_common: ', new_schedules_common)
+      db.session.add(new_schedules_common)
+      db.session.commit()
+      response_object = {
+        'status': 'OK',
+        'message': 'Successfully get monthly checked.',
+      }
+      return response_object, 200
+    else:
+      response_object = {
+        'status': 'fail',
+        'message': 'Provide a valid auth token.',
+      }
+      return response_object, 401
+      
+  except Exception as e:
+      response_object = {
+        'status': 'Internal Server Error',
+        'message': 'Some Internal Server Error occurred.',
+      }
+      return response_object, 500

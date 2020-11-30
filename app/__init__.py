b7fab2b57bb7091dae2ac68db791e2eea941ccbb
Grace Chung <hjngy0511@gmail.com>
2020-11-30 14:43:17 +0900

    fix/:bulb:/schedules-date로 uri 수정

--- app/__init__.py
+++ app/__init__.py
@@ -14,6 +14,6 @@ api = Api(blueprint,
           )
 
 
-api.add_namespace(schedules_date, path='/schedules_dates')
+api.add_namespace(schedules_date, path='/schedules-dates')
 api.add_namespace(oauth, path="/oauth/kakao") 
 api.add_namespace(test)

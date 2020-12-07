8c51adb2c108387d619ba9ecb27aa241d9cf9fb0
Grace Chung <hjngy0511@gmail.com>
2020-12-07 09:27:13 +0900

    review/fix/wee1,2 API done - get monthly checked API 수정된 DB사항 반영해서 정상 작동 완료 solved issue - 실제 존재하지 않는 날짜인 경우 형변환이 되지 않고 에러가 발생 client가 주는 end_day를 다음달 1일로 하여 해결 solved issue - 401에러 발생 분기점 수정

--- app/main/service/schedules_date.py
+++ app/main/service/schedules_date.py
@@ -6,7 +6,7 @@ from sqlalchemy import and_
 import json
 import jwt
 import re
-from datetime import time
+import datetime
 from operator import itemgetter
 from app.main import db
 from app.main.model.schedules_date import Schedules_date
@@ -18,12 +18,19 @@ import re
 
 class TimeFormat(fields.Raw):
     def format(self, value):
-        return time.strftime(value, "%H:%M")
+        return datetime.time.strftime(value, "%H:%M")
 
-def sorting_date_time(data):
+def sorting_alarmdate_time(data):
   """ date로 오름차순 정렬 후 time으로 오름차순 정렬하는 함수 """
-  data = sorted(data, key=itemgetter('date', 'time'))
+  data = sorted(data, key=itemgetter('alarmdate', 'time'))#그래서 이걸 data = sorted(data, key=itemgetter('alarmdate', 'time'))로 고쳐주면 될 듯함
   return data
+# def sorting_alarmdate_time(data):
+#   """ DD로 오름차순 정렬 후 time으로 오름차순 정렬하는 함수 """
+#   alarmdate = re.split('-', Schedules_date.alarmdate) 
+#   data = sorted(alarmdate, key=itemgetter(3, 'time')) 
+# split할 필요없이 로직은 기존과 동일하게
+# alramdat 오름차순 정렬 후 time으로 정렬하면 됩니다.
+
 
 
 def sorting_time(data):
@@ -34,30 +41,33 @@ def sorting_time(data):
 
 def get_monthly_checked(data): 
   """ Get monthly checked API for calendar"""
-  try:
-    parsing = data['month'].split('-') 
-    year = parsing[0] 
-    month = parsing[1] 
+  try: 
+    start_day = datetime.datetime.strptime(data['start_day'], '%Y-%m-%d')
+    end_day = datetime.datetime.strptime(data['end_day'], '%Y-%m-%d')
+    try: 
+      token = request.headers.get('Authorization')
 
-    token = request.headers.get('Authorization')
-    decoded_token = jwt.decode(token, jwt_key, jwt_alg)
-    user_id = decoded_token['id']
+      decoded_token = jwt.decode(token, jwt_key, jwt_alg)
+      user_id = decoded_token['id']
 
-    if decoded_token:
-      topic_fields = {
-        'date': fields.Integer(required=True),
-        'time': TimeFormat(readonly=True, description='Time in HH:MM', default='HH:MM'),
-        'check': fields.Boolean(required=True),
-      }
-      data = [marshal(topic, topic_fields) for topic in Schedules_date.query.filter(and_(Schedules_date.year==year, Schedules_date.month==month, Schedules_date.user_id==user_id)).all()]
-      results = sorting_date_time(data)
-      response_object = {
-        'status': 'OK',
-        'message': 'Successfully get monthly checked.',
-        'results': results
-      }
-      return response_object, 200
-    else:
+      if decoded_token:
+        topic_fields = {
+          'alarmdate': fields.Date(required=True),
+          'time': TimeFormat(readonly=True, description='Time in HH:MM', default='HH:MM'),
+          'check': fields.Boolean(required=True),
+        }
+      
+        data = [marshal(topic, topic_fields) for topic in Schedules_date.query
+                                                                        .filter(and_(Schedules_date.alarmdate>=start_day, Schedules_date.alarmdate<end_day, Schedules_date.user_id==user_id))#and_(Schedules_date.alarmdate.between(start_day, end_day), 
+                                                                        .all()]
+        results = sorting_alarmdate_time(data)
+        response_object = {
+          'status': 'OK',
+          'message': 'Successfully get monthly checked.',
+          'results': results
+        }
+        return response_object, 200
+    except Exception as e:
       response_object = {
         'status': 'fail',
         'message': 'Provide a valid auth token.',
@@ -72,11 +82,6 @@ def get_monthly_checked(data):
     return response_object, 500
 
 
-"""
-client가 params에 
-today: 2020-11-22 12:23:00
-와 같은 형태로 get 요청을 한다는 가정하에 구현
-"""
 def get_alarms_list(data): 
   """ Get Alarms List on Clicked date for main page and calendar page"""
   try:

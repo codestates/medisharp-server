0ff281acfcaf9dc78c7bb6894f8daabc04952159
Grace Chung <hjngy0511@gmail.com>
2020-12-10 23:24:06 +0900

    #9/:bulb:/PredictMedicineNameAPI done1 - 학습시킨 후 저장된 모델을 load하여 받아온 이미지를 input으로 예측한 이름을 응답하는 API 구현 완료 done2 - 동일한 코드로 배포환경에서도 작동하는 것 확인 완료 done3 - client와 통신 확인 완료

--- app/main/controller/medicines.py
+++ app/main/controller/medicines.py
@@ -9,79 +9,83 @@ from keras.preprocessing.image import img_to_array
 from PIL import Image
 import requests
 import numpy as np
-import cv2
+# import cv2
 import os
 import sys
 import io
+import jwt
 
+from ..config import jwt_key, jwt_alg
 from ..util.dto import MedicineDto
 from ..service.medicines import post_medicine, post_schedules_common_medicines, upload_medicine
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))
+from cnn.class_list import get_class_list
 
 api = MedicineDto.api
 # _medicines = MedicineDto.medicines
 
-# 2단계 상위폴더 경로를 추가하는 방법: https://brownbears.tistory.com/296
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))
-from cnn.class_list import get_class_list
-
-# def load_model():
-# 	global model
-# 	model = load_model('/Users/jeonghyeonjeong/Desktop/medisharp-server/cnn/Pill_image_pretrained_mobile_model_2.h5')
-
-
 def prepare_image(image, target):
-	# if the image mode is not RGB, convert it
 	if image.mode != "RGB":
 		image = image.convert("RGB")
 
-	# resize the input image and preprocess it
 	image = image.resize(target)
 	image = img_to_array(image)
 	image = np.expand_dims(image, axis=0)
 	image = imagenet_utils.preprocess_input(image)
 
-	# return the processed image
 	return image
 
 @api.route('/image')
-class ImageUpload(Resource):
+class PredictMedicineName(Resource):
   def post(self):
-    """카메라로 촬영한 이미지를 서버로 보내오고, 학습된 모델에서 예측결과를 client에게 전달해준는 API"""
-    print("알약 list: ", get_class_list()) #잘 찍힌다.
-    class_list =  get_class_list()
-    print("request: ", request.files)
-    if 'image' not in request.files:
-        print('No File Part')
-    file = request.files['image']
-    if file.filename == '':
-        print('No Selected File')
-    elif file and file.filename:
-      image = flask.request.files["image"].read()
-      image = Image.open(io.BytesIO(image))
+    """카메라로 촬영한 이미지를 서버로 보내오고, 학습된 모델에서 예측결과를 client에게 전달해주는 API"""
+    try: 
+      class_list =  get_class_list()
+      try:
+        token = request.headers.get('Authorization')
+        decoded_token = jwt.decode(token, jwt_key, jwt_alg)
+        user_id = decoded_token['id']
 
-      # preprocess the image and prepare it for classification 
-      image = prepare_image(image, target=(224, 224))
+        if 'image' not in request.files:
+            print('No File Part')
+        file = request.files['image']
+        if file.filename == '':
+            print('No Selected File')
+        elif file and file.filename:
+          image = flask.request.files["image"].read()
+          image = Image.open(io.BytesIO(image))
+          image = prepare_image(image, target=(224, 224))
 
-      print("현재 실행경로: ", os.getcwd())
-      modeldir = os.path.join(currdir+ "/cnn/Pill_image_pretrained_mobile_model_2.h5")
-      print("모델 실행 경로: ", modeldir)
-      model = load_model(modeldir)
-      
-      # classify the input image and then initialize the list
-			# of predictions to return to the client
-      preds = model.predict(image)
-      preds = model.predict(image)
-			
-      pred_class = np.argmax(preds, axis=-1)
-      prediction_result = class_list[int(pred_class)]
-      print("prediction: ", class_list[int(pred_class)])#prediction:  이연클래리트로마이신정500밀리그램   
+          currdir = os.getcwd()
+          modeldir = os.path.join(currdir+ "/cnn/Pill_image_pretrained_mobile_model_2.h5")
+          model = load_model(modeldir)
 
-      response_object = {
-        'status': 'OK',
-        'message': 'Successfully predict image class.',
-        'prediction': prediction_result
-      }
-      return response_object, 200
+          preds = model.predict(image)
+          pred_class = np.argmax(preds, axis=-1)
+          prediction_result = class_list[int(pred_class)]
+          print("prediction: ", class_list[int(pred_class)])
+
+          response_object = {
+            'status': 'OK',
+            'message': 'Successfully predict image class.',
+            'prediction': prediction_result
+          }
+          return response_object, 200
+
+      except Exception as e:  
+        response_object = {
+          'status': 'fail',
+          'message': 'Provide a valid auth token.',
+        }
+        return response_object, 401
+
+    except Exception as e:
+        response_object = {
+          'status': 'Internal Server Error',
+          'message': 'Some Internal Server Error occurred.',
+        }
+        return response_object, 500
+
 
 @api.route('')
 class PostMedicine(Resource):
@@ -108,11 +112,10 @@ class UploadMedicine(Resource):
       print('type:',file.content_type)
       #print('filestr:',filestr)
       return upload_medicine(file)
-
+      
 @api.route('/schedules-medicines')
 class PostSchedulesCommonMedicines(Resource):
   def post(self):
     """Post Schedules Common Medicines API"""
     data = request.get_json().get('schedules_common_medicines')
     return post_schedules_common_medicines(data)
-

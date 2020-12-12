53617680ab153801689a6a96c944bc7e08a1c0a8
hyunjusong <hun3780@gmail.com>
2020-12-12 18:02:47 +0900

    기존에 저장된 scheduels_date Db 에서 오늘날짜 이후의 데이터 우선 지우기

--- app/main/controller/schedules_common.py
+++ app/main/controller/schedules_common.py
@@ -3,7 +3,7 @@ from flask_restx import Resource
 
 from ..util.dto import Schedules_commonDto
 import requests
-from ..service.schedules_common import post_schedules_common, post_schedules_date
+from ..service.schedules_common import post_schedules_common, post_schedules_date , edit_schedules_date
 
 api = Schedules_commonDto.api
 _schedules_common = Schedules_commonDto.schedules_common
@@ -15,24 +15,16 @@ class PostSchedulesCommon(Resource):
     data = request.get_json().get('schedules_common') 
     return post_schedules_common(data) 
 
-"""
-client에서 
-{
-  "schedules_common":
-    {
-      "medicine_id": [1, 2, 3],
-      "schedules_common_id": 1, 
-      "time": 11:30:00
-    }
-}
-을 request에 준다고 가정하고 짠다. 
-"""
-
 
 @api.route('/schedules-dates') 
-class PostSchedulesDate(Resource):
+class SchedulesDate(Resource):
   def post(self):
     """Post Schedules Date API"""
     data = request.get_json().get('schedules_common') 
     return post_schedules_date(data) 
+
+  def patch(self):
+    """Edit Schedules Date API"""
+    data = request.get_json().get('schedules_common') 
+    return edit_schedules_date(data) 
 
--- app/main/service/schedules_common.py
+++ app/main/service/schedules_common.py
@@ -13,6 +13,7 @@ from app.main.model.schedules_date import Schedules_date
 from app.main.model.users import Users
 from ..config import jwt_key, jwt_alg
 import re
+from sqlalchemy.sql.functions import now
 
 def post_schedules_common(data):
   """ Post Common information of alarm"""
@@ -112,6 +113,76 @@ def post_schedules_date(data):
           'message': 'Provide a valid auth token.',
         }
         return response_object, 401
+
+  except Exception as e:
+      response_object = {
+        'status': 'Internal Server Error',
+        'message': 'Some Internal Server Error occurred.',
+      }
+      return response_object, 500
+
+def edit_schedules_date(data):
+  """ Post Schedules Date API"""
+  try:
+    cycle = data['cycle']
+    schedules_common_id = data['schedules_common_id']
+    time = data['time']
+
+    try:
+      token = request.headers.get('Authorization')
+      decoded_token = jwt.decode(token, jwt_key, jwt_alg)
+      user_id = decoded_token['id']
+
+      if decoded_token:
+        data = db.session.query(Schedules_common.startdate, Schedules_common.enddate, Schedules_common.cycle).filter(and_(Schedules_common.id==schedules_common_id, Schedules_common.user_id==user_id)).all() 
+        startdate = datetime.datetime.strptime(data[0].startdate, '%Y-%m-%d')
+        enddate = datetime.datetime.strptime(data[0].enddate, '%Y-%m-%d')
+        cycle = data[0].cycle #2
+        print(startdate, enddate, cycle)
+        #여기까지 post와 동일
+        
+        currdate = now.strftime('%Y-%m-%d %H:%M:%S') #오늘자 기준으로 for loop 돌리기
+        # 1. 우선 DB내에서 오늘이후의 schedules_date 지우기
+        while currdate <=enddate:
+          saved_schedule = Schedules_date.query.filter_by(alarmdate=currdate).first()
+          db.session.delete(saved_schedule)
+          db.session.commit()
+          currdate = currdate + datetime.timedelta(days=cycle)
+
+        
+        # while currdate <= enddate:
+        #   #이를 schedules_date 테이블에 넣어주기
+        #   new_schedules_date = Schedules_date(
+        #     alarmdate = currdate,
+        #     time = time,
+        #     check = 0,
+        #     user_id = user_id,
+        #     schedules_common_id = schedules_common_id
+        #   )
+        #   db.session.add(new_schedules_date)
+        #   db.session.commit()
+        #   currdate = currdate + datetime.timedelta(days=cycle)
+
+        # #response는 medicine_id와 schedules_common_id
+        # results = {
+        #   'medicine_id': medicine_id,
+        #   'schedules_common_id': schedules_common_id
+        # }
+
+        # response_object = {
+        #   'status': 'OK',
+        #   'message': 'Successfully post schedules date.',
+        #   'results': results
+        # }
+        # return response_object, 200
+
+    except Exception as e:
+      print(e)
+      response_object = {
+        'status': 'fail',
+        'message': 'Provide a valid auth token.',
+      }
+      return response_object, 401
 
   except Exception as e:
       response_object = {

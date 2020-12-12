b72a7d36d12f265dc6e1ca9c26cf3fd0e0a7acfe
hyunjusong <hun3780@gmail.com>
2020-12-12 19:37:37 +0900

    feature/:bulb:/delete all schedules common/date 경로 설정 / 클릭한 날짜만 삭제되는 경우 고려한 controller 분기 지정

--- app/main/controller/schedules_common.py
+++ app/main/controller/schedules_common.py
@@ -3,7 +3,7 @@ from flask_restx import Resource
 
 from ..util.dto import Schedules_commonDto
 import requests
-from ..service.schedules_common import post_schedules_common, post_schedules_date
+from ..service.schedules_common import post_schedules_common, post_schedules_date , delete_all_schedules
 
 api = Schedules_commonDto.api
 _schedules_common = Schedules_commonDto.schedules_common
@@ -16,9 +16,15 @@ class PostSchedulesCommon(Resource):
     return post_schedules_common(data) 
 
 @api.route('/schedules-dates') 
-class PostSchedulesDate(Resource):
+class SchedulesDate(Resource):
   def post(self):
     """Post Schedules Date API"""
     data = request.get_json().get('schedules_common') 
     return post_schedules_date(data) 
+
+  def delete(self):
+    """delete Schedules Date API"""
+    data = request.args.to_dict()
+    if 'date' not in data.keys():
+      return delete_all_schedules(data)
 

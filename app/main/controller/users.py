7d495f8cffad3cecdf41861a476c59a601db5ed0
hyunjusong <hun3780@gmail.com>
2020-12-19 19:21:58 +0900

    fix/:bulb:/socialLoginRefactoring /같은 dto를 다른 controller에서 쓸수 없는 경우로 인해 기존의 소셜로그인을 users api안에 포함 했습니다

--- app/main/controller/users.py
+++ app/main/controller/users.py
@@ -8,12 +8,12 @@ from flask_restx import Resource
 
 from ..util.dto import UserDto
 import requests
-from ..service.oauth import social_signin
+from ..service.users import social_signin
 from ..config import kakao_client_id
 
 api = UserDto.api
 
-@api.route("/") # 카카오 로그인하기를 누르면 우선 해당 api로 오게되고, redirect를 통해 바로 request token을 받기 위해 링크가 변경됩니다.
+@api.route("/oauth/kakao") 
 class KakaoSignIn(Resource):
     def get(self):
         client_id = kakao_client_id
@@ -22,35 +22,27 @@ class KakaoSignIn(Resource):
         return redirect(kakao_oauthurl)
 
 
-@api.route("/callback") # 위의 라우팅에서 redirect 되면서 바로 이 api로 오게됩니다.
+@api.route("/oauth/kakao/callback") 
 class KakaoSignInCallback(Resource):
     def get(self):
         #print("request.GET :" , request.args.get)
         try:
-            code = request.args.get("code")  # callback 뒤에 붙어오는 request token을 뽑아내 줍니다.                                    
+            code = request.args.get("code")                                 
             client_id = kakao_client_id
             redirect_uri = "http://127.0.0.1:5000/oauth/kakao/callback"
             
-            #print("code :" , code)
-
-            #Python에서 HTTP 요청을 보내는 모듈인 requests
-            #node보다 장점을 드디어 찾았어요!
-            #node에선 서버를 클라화 해주기 위해 아씨오를 부르고 이래저래 했는데, flask에서는 requests 라는 모듈을 써서 바로 해당 url로 get 요청을 보낼수있어요
             token_request = requests.get(                                       
                 f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={client_id}&redirect_uri={redirect_uri}&code={code}"
             )
             
-            token_json = token_request.json()  # 위의 get 요청을 통해 받아온 데이터를 json화 해주면 이곳에 access token 이 숨어있습니다.
-
-            #print("token_json:", token_json)                                
-            
+            token_json = token_request.json() 
 
             error = token_json.get("error",None)
 
             if error is not None :
-                return make_response({"message": "INVALID_CODE"}, 400) #에러 처리 한번 해주고
+                return make_response({"message": "INVALID_CODE"}, 400) 
 
-            access_token = token_json.get("access_token") #카카오 소셜로그인을 통해 유저에 대한 정보를 받을 권한이 있는 토큰이 이것입니다. 
+            access_token = token_json.get("access_token") 
             #print("access_token:", access_token)                       
 
             profile_request = requests.get(

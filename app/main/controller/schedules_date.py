8a33af21726811306d39118d06c7913dd82747ba
hyunjusong <hun3780@gmail.com>
2021-01-13 20:15:43 +0900

    schedule_dates 관련 코드 1차 refactoring 완료 1. 응답 메시지들이 반복되는 것을 함수화 해주면 좋을거 같아서 아예 모듈화하였습니다. 2. calendar의 month 데이터들의 경우는 각 api가 최초에만 같이 호출되지 이후엔 따로 움직여야 하는 것이라 합치지 않았습니다

--- app/main/controller/schedules_date.py
+++ app/main/controller/schedules_date.py
@@ -2,34 +2,82 @@ from flask import request, redirect, jsonify, make_response
 from flask_restx import Resource
 
 from ..util.dto import Schedules_dateDto
-import requests
+import requests, jwt
 from ..service.schedules_date import get_monthly_checked, get_alarms_list, get_today_checked, patch_check
 from app.main import db
+from ..config import jwt_key, jwt_alg
 
 api = Schedules_dateDto.api
 _schedules_date = Schedules_dateDto.schedules_date
+
+
+def response_ok(result):
+  response_object = { 
+    'status': 'OK', 
+    'message': 'Successfully get today checked.',
+    'results': result
+  }
+  return response_object
+
+def response_err():
+  response_object = {
+    'status': 'Internal Server Error',
+    'message': 'Some Internal Server Error occurred.',
+  }
+  return response_object
+
 
 @api.route('/check')
 class PatchCheck(Resource):
   def patch(self):
     """Patch Check API"""
-    data = request.get_json().get('schedules_common') 
-    return patch_check(data)
+    try:
+      token = request.headers.get('Authorization')
+      decoded_token = jwt.decode(token, jwt_key, jwt_alg)
+      user_id = decoded_token['id']
+      if user_id:
+        data = request.get_json().get('schedules_common') 
+        result = patch_check(data, user_id)
+        db.session.commit()
+        return response_ok(result), 200
+    except Exception as e:
+      db.session.rollback()
+      raise
+      return response_err(), 500
+    finally:
+      db.session.close()
+
 
 @api.route('/check/month') 
 class Check(Resource):
   def get(self):
     """Get Montly Checked API"""
-    data = request.args.to_dict()
-    return get_monthly_checked(data)
+    try:
+      token = request.headers.get('Authorization')
+      decoded_token = jwt.decode(token, jwt_key, jwt_alg)
+      user_id = decoded_token['id']   
+      if user_id: 
+        params = request.args.to_dict()
+        result =  get_monthly_checked(params, user_id)
+        return response_ok(result), 200
+    except Exception as e:
+      return response_err(), 500
 
 
 @api.route('/schedules-commons/alarm')
 class TodayAlarmList(Resource):
   def get(self):
     """Get Alarms List on Clicked date"""
-    data = request.args.to_dict()
-    return get_alarms_list(data)
+    try:
+      token = request.headers.get('Authorization')
+      decoded_token = jwt.decode(token, jwt_key, jwt_alg)
+      user_id = decoded_token['id']
+      if user_id:
+        params = request.args.to_dict()
+        result = get_alarms_list(params, user_id)
+        return response_ok(result), 200
+    except Exception as e:
+      return response_err(), 500
   
 
 @api.route('/today') 
@@ -37,21 +85,15 @@ class Check(Resource):
   def get(self):
     """Get Today Alarm API"""
     try:
-      req = request.args.to_dict()
-      result = {'today_check' : get_today_checked(req) , 'today_alarm' : get_alarms_list(req)}
-      print(result)
-      response_object = { 
-          'status': 'OK', 
-          'message': 'Successfully get today checked.',
-          'results': result
-        }
-      return response_object, 200
+      token = request.headers.get('Authorization')
+      decoded_token = jwt.decode(token, jwt_key, jwt_alg)
+      user_id = decoded_token['id']
+      if user_id:
+        params = request.args.to_dict()
+        result = {'today_check' : get_today_checked(params, user_id) , 'today_alarm' : get_alarms_list(params, user_id)}
+        return response_ok(result), 200
     except Exception as e:
-      response_object = {
-        'status': 'Internal Server Error',
-        'message': 'Some Internal Server Error occurred.',
-      }
-      return response_object, 500
+      return response_err(), 500
       
 
 

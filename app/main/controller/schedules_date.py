68109b6294ee87cf8d32769bf02c79732c1e5225
hyunjusong <hun3780@gmail.com>
2021-01-13 19:07:12 +0900

    home screen에서 today api 주소 하나로 두개의 데이터를 다 받아올 수 있도록 constroller에서 최종적으로 합쳐서 client에 전달 / service에서 최대한 join을 해보려햇으나, 여기까진 아직 진행하지 못했고(가능한지에 대해서도 쿼리문을 좀더 살펴볼 필요가 있을거 같습니다.

--- app/main/controller/schedules_date.py
+++ app/main/controller/schedules_date.py
@@ -4,20 +4,11 @@ from flask_restx import Resource
 from ..util.dto import Schedules_dateDto
 import requests
 from ..service.schedules_date import get_monthly_checked, get_alarms_list, get_today_checked, patch_check
+from app.main import db
 
 api = Schedules_dateDto.api
 _schedules_date = Schedules_dateDto.schedules_date
 
-"""
-client에서 
-{
-  "schedules_common": 
-    {"schedules_common_id": 1, 
-      "clickdate": '2020-12-12',
-    }
-}
-의 형태로 보내준다고 생각하고 구현
-"""
 @api.route('/check')
 class PatchCheck(Resource):
   def patch(self):
@@ -41,12 +32,27 @@ class TodayAlarmList(Resource):
     return get_alarms_list(data)
   
 
-@api.route('/check/today') 
+@api.route('/today') 
 class Check(Resource):
   def get(self):
-    """Get Today Checked API"""
-    data = request.args.to_dict()
-    return get_today_checked(data)
+    """Get Today Alarm API"""
+    try:
+      req = request.args.to_dict()
+      result = {'todayCheck' : get_today_checked(req) , 'todayAlarm' : get_alarms_list(req)}
+      print(result)
+      response_object = { 
+          'status': 'OK', 
+          'message': 'Successfully get today checked.',
+          'results': result
+        }
+      return response_object, 200
+    except Exception as e:
+      response_object = {
+        'status': 'Internal Server Error',
+        'message': 'Some Internal Server Error occurred.',
+      }
+      return response_object, 500
+      
 
 
 
